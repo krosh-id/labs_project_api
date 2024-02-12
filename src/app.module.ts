9eace541326c8346c346e09298c78b5cd3bf25d8
Vlad <invitegoogl@gmail.com>
2024-02-12 15:49:58 +0300

    Bearer authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,6 @@ import { LabsModule } from './labs/labs.module';
 import { UsersModule } from './users/users.module';
 import { User } from "./users/users.model";
 import { Labs } from "./labs/labs.models";
-import { RolesController } from './roles/roles.controller';
-import { RolesService } from './roles/roles.service';
 import { RolesModule } from './roles/roles.module';
 import { Role } from "./roles/roles.model";
 import { UserRoles } from "./roles/user-roles.model";
